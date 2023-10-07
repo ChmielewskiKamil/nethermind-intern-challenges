d3ba62e187061f9d7b4a03919b026b3480033c6a
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-10-07 15:55:03 +0200

    chore: Return empty every time

--- src/agent.ts
+++ src/agent.ts
@@ -1,1 +1,11 @@
+import { HandleTransaction } from "forta-agent";
+
 export const NETHERMIND_DEPLOYER_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
+
+const handleTransaction: HandleTransaction = async (txEvent) => {
+  return [];
+};
+
+export default {
+  handleTransaction,
+};
