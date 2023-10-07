f062fde983b4ca843c3551aaea77b89e0a7bb485
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-10-07 15:54:12 +0200

    test: Test empty findings when no update/creation

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -1,7 +1,20 @@
-import { NETHERMIND_DEPLOYER_ADDRESS } from "./agent";
+import { createTransactionEvent, HandleTransaction } from "forta-agent";
+import agent, { NETHERMIND_DEPLOYER_ADDRESS } from "./agent";
 
 describe("nethermind bot creation and update monitoring agent", () => {
+  let handleTransaction: HandleTransaction;
+  const mockTxEvent = createTransactionEvent({} as any);
+
+  beforeAll(() => {
+    handleTransaction = agent.handleTransaction;
+  });
+
   it("should use correct nethermind deployer address", async () => {
     expect(NETHERMIND_DEPLOYER_ADDRESS).toStrictEqual("0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8");
+  });
+
+  it("returns empty findings when no bot created or updated in a tx", async () => {
+    const findings = await handleTransaction(mockTxEvent);
+    expect(findings).toStrictEqual([]);
   });
 });
