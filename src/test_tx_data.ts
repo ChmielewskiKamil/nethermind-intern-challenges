bb5a9229794597d2b54357e1da7e86af168e82e3
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-10-08 14:44:11 +0200

    chore: Add bot creation test TX

--- src/test_tx_data.ts
+++ src/test_tx_data.ts
@@ -8,3 +8,8 @@ export const NETHERMIND_BOT_UPDATE_TX = "0xc6a4f95f3bab231a7422f4c2424645104e81d
 // - DOES NOT update the Forta Bot
 // - It Enables/Disables the Forta Bot
 export const NETHERMIND_BOT_ENABLE_DISABLE_TX = "0x1f63262d5d114b92cee2ff38bd242ca383d65f17dcf23e2858f3c18e2ba253d6";
+
+// https://polygonscan.com/tx/0x4d4c1a71122050441129c656cc7c239753b3cbc278d1850a9a8a7c745818addc
+// This is a TX originating from the Nethermind deployer that:
+// - Creates the Forta Bot
+export const NETHERMIND_BOT_CREATE_TX = "0x4d4c1a71122050441129c656cc7c239753b3cbc278d1850a9a8a7c745818addc";
