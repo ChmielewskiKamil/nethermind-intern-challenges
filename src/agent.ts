ccf948e35d943ee80758625cc8e97e64be3dcb58
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-10-08 16:44:57 +0200

    refactor: Move createFinding func to utils
    chore: Format code

--- src/agent.ts
+++ src/agent.ts
@@ -1,12 +1,6 @@
-import {
-  Finding,
-  FindingSeverity,
-  HandleTransaction,
-  FindingType,
-  TransactionEvent,
-  LogDescription,
-} from "forta-agent";
+import { Finding, HandleTransaction, TransactionEvent } from "forta-agent";
 import { EVENT_SIGS_TO_MONITOR, NETHERMIND_DEPLOYER_ADDRESS } from "./constants";
+import { createFinding } from "./utils";
 
 const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
   const findings: Finding[] = [];
@@ -21,33 +15,4 @@ const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) =
 
 export default {
   handleTransaction,
-};
-
-// Forked from @0xtaf's
-// https://github.com/NethermindEth/Forta-Agents/blob/main/compound-comptroller-bot/src/finding.ts
-export const createFinding = (log: LogDescription): Finding => {
-  switch (log.signature) {
-    case "Transfer(address,address,uint256)":
-      return Finding.from({
-        name: "Forta Agent has been created",
-        description: `Forta Agent has been created by ${log.args[1]}`,
-        alertId: "NETH-FORTA-BOT-CREATE",
-        protocol: "Forta",
-        type: FindingType.Info,
-        severity: FindingSeverity.Info,
-        metadata: {},
-        addresses: [log.address],
-      });
-    default:
-      return Finding.from({
-        name: "Forta Agent has been updated",
-        description: `Forta Agent ${log.args[0]} has been updated by ${log.args[1]}`,
-        alertId: "NETH-FORTA-BOT-UPDATE",
-        protocol: "Forta",
-        type: FindingType.Info,
-        severity: FindingSeverity.Info,
-        metadata: {},
-        addresses: [log.address],
-      });
-  }
 };
