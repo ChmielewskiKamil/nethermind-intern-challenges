508b4e5df5ac1b651db30be9b95653be33dc8259
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-10-08 12:53:05 +0200

    docs: Explain test TX in detail

--- src/test_tx_data.ts
+++ src/test_tx_data.ts
@@ -1,2 +1,4 @@
 // https://polygonscan.com/tx/0xc6a4f95f3bab231a7422f4c2424645104e81de1415c07fc7538cbdb4b8dfa97b#eventlog
+// This is a TX originating from the Nethermind deployer that:
+// - Updates the Forta Bot
 export const NETHERMIND_BOT_UPDATE_TX = "0xc6a4f95f3bab231a7422f4c2424645104e81de1415c07fc7538cbdb4b8dfa97b";
