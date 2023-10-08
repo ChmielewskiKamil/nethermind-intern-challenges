653964c491516c881c0f0c0842920f3e15cdfda5
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-10-08 13:01:12 +0200

    chore: Add test TX with NO update & NO creation

--- src/test_tx_data.ts
+++ src/test_tx_data.ts
@@ -1,4 +1,10 @@
-// https://polygonscan.com/tx/0xc6a4f95f3bab231a7422f4c2424645104e81de1415c07fc7538cbdb4b8dfa97b#eventlog
+// https://polygonscan.com/tx/0xc6a4f95f3bab231a7422f4c2424645104e81de1415c07fc7538cbdb4b8dfa97b
 // This is a TX originating from the Nethermind deployer that:
 // - Updates the Forta Bot
 export const NETHERMIND_BOT_UPDATE_TX = "0xc6a4f95f3bab231a7422f4c2424645104e81de1415c07fc7538cbdb4b8dfa97b";
+
+// https://polygonscan.com/tx/0x1f63262d5d114b92cee2ff38bd242ca383d65f17dcf23e2858f3c18e2ba253d6
+// This is a TX originating from the Nethermind deployer that:
+// - DOES NOT update the Forta Bot
+// - It Enables/Disables the Forta Bot
+export const NETHERMIND_BOT_ENABLE_DISABLE_TX = "0x1f63262d5d114b92cee2ff38bd242ca383d65f17dcf23e2858f3c18e2ba253d6";
