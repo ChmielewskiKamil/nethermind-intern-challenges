dbff09762ddf696dbca08c0a0bbcf03b58b1fa66
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-10-08 18:43:49 +0200

    audit: BUG when interacting with other contracts
    chore: Format docs

--- src/agent.ts
+++ src/agent.ts
@@ -5,6 +5,8 @@ import { createFinding } from "./utils";
 const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
   const findings: Finding[] = [];
 
+  /* @audit-issue If the deployer interacts with different contract that emits the same event signature,
+   * it will trigger an alert. It is not a correct behaviour. */
   // Why can't I use getEvent from the interface?
   txEvent.filterLog(EVENT_SIGS_TO_MONITOR, NETHERMIND_DEPLOYER_ADDRESS).forEach((log) => {
     findings.push(createFinding(log));
