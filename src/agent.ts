65bcc91e5fee95b9e1d15ac2a20dd973f2bbdf9b
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-10-08 12:37:40 +0200

    feat: Add naive impl of update bot finding
    chore: Format code

--- src/agent.ts
+++ src/agent.ts
@@ -1,9 +1,27 @@
-import { HandleTransaction } from "forta-agent";
+import { Finding, FindingSeverity, HandleTransaction, FindingType } from "forta-agent";
 
 export const NETHERMIND_DEPLOYER_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
 
 const handleTransaction: HandleTransaction = async (txEvent) => {
-  return [];
+  const findings: Finding[] = [];
+
+  const HARDCODED_EVENT_SIG = "event AgentUpdated(uint256,address,string,uint256[])";
+  console.log(txEvent.logs);
+  // Why can't I use getEvent from the interface?
+  const fortaBotUpdateEvents = txEvent.logs;
+
+  fortaBotUpdateEvents.forEach((updateEvent) => {
+    findings.push(
+      Finding.fromObject({
+        name: "Nethermind Deployer - Bot Update",
+        description: "Nethermind Deployer has updated a Forta bot",
+        alertId: "NETH-FORTA-BOT-UPDATE",
+        severity: FindingSeverity.Low,
+        type: FindingType.Info,
+      })
+    );
+  });
+  return findings;
 };
 
 export default {
