598ba9d2115f6c836e0734f94db904b03cd1e784
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-10-08 12:54:45 +0200

    refactor: Add TxEvent type to improve type system

--- src/agent.ts
+++ src/agent.ts
@@ -1,8 +1,8 @@
-import { Finding, FindingSeverity, HandleTransaction, FindingType } from "forta-agent";
+import { Finding, FindingSeverity, HandleTransaction, FindingType, TransactionEvent } from "forta-agent";
 
 export const NETHERMIND_DEPLOYER_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
 
-const handleTransaction: HandleTransaction = async (txEvent) => {
+const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
   const findings: Finding[] = [];
 
   // const HARDCODED_EVENT_SIG = "event AgentUpdated(uint256,address,string,uint256[])";
