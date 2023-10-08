c49efa431e9828512a39c8d1053984774761447f
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-10-08 15:02:06 +0200

    test: Make bot update test stricter

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -63,7 +63,19 @@ describe("nethermind bot creation and update monitoring agent", () => {
       .addEventLog(eventFragment, sender, inputs);
 
     const findings = await handleTransaction(txEvent);
-    expect(findings).toHaveLength(1);
+    expect(findings).toStrictEqual([
+      Finding.fromObject({
+        name: "Forta Agent has been updated",
+        description:
+          "Forta Agent 63178469883724842253811149026434419297883949962197771239560976121248185588756 has been updated by 0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8",
+        alertId: "NETH-FORTA-BOT-UPDATE",
+        protocol: "Forta",
+        type: FindingType.Info,
+        severity: FindingSeverity.Info,
+        metadata: {},
+        addresses: ["0x88dc3a2284fa62e0027d6d6b1fcfdd2141a143b8"],
+      }),
+    ]);
   });
 
   it("should distinguish between bot creation and bot update", async () => {
