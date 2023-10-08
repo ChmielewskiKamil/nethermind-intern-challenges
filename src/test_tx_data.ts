5cf9f9a88ccd3ba4325034269e0f883ba06cad27
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-10-08 12:49:54 +0200

    chore: Create file for test TX data handling

---
+++ src/test_tx_data.ts
@@ -0,0 +1,2 @@
+// https://polygonscan.com/tx/0xc6a4f95f3bab231a7422f4c2424645104e81de1415c07fc7538cbdb4b8dfa97b#eventlog
+export const NETHERMIND_BOT_UPDATE_TX = "0xc6a4f95f3bab231a7422f4c2424645104e81de1415c07fc7538cbdb4b8dfa97b";
